6763100abd78ce3ba2176a3c7ea70148b816b18a
Mannuc-90 <manpreet.c1990@gmail.com>
2022-06-29 01:36:50 +0530

    AAdded recover

--- src/main/java/com/example/retry/proxy/Proxy.java
+++ src/main/java/com/example/retry/proxy/Proxy.java
@@ -2,6 +2,7 @@ package com.example.retry.proxy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -15,10 +16,17 @@ public class Proxy {
 	@Retryable(value = RuntimeException.class, maxAttempts = 4, backoff = @Backoff(multiplier = 2))
 	public String retry(String pathVar) {
 
-		System.out.println("entering retry :  path var : "+pathVar);
-		if(pathVar.equals("err")) {
+		System.out.println("entering retry :  path var : " + pathVar);
+		if (pathVar.equals("err")) {
 			throw new RuntimeException();
 		}
-		return restTemplate.getForObject("http://httpbin.org/delay/3",String.class);
+
+		return restTemplate.getForObject("http://httpbin.org/delay/3", String.class);
+	}
+	
+	@Recover
+	public String recover() {
+		System.out.println("Requuest failed");
+		return "Recover";
 	}
 }
