eaed275d88a3fe8e4fd13a872bb6df9b4a764765
Mannuc-90 <manpreet.c1990@gmail.com>
2022-06-28 18:44:45 +0530

    retry

---
+++ src/main/java/com/example/retry/controller/Controller.java
@@ -0,0 +1,20 @@
+package com.example.retry.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.retry.proxy.Proxy;
+
+@RestController
+public class Controller {
+
+	@Autowired
+	private Proxy proxy;
+	
+	@GetMapping("/retry/{pathVar}")
+	public String retryEvent(@PathVariable String pathVar) {
+		return proxy.retry(pathVar);
+	}
+}
---
+++ src/main/java/com/example/retry/proxy/Proxy.java
@@ -0,0 +1,24 @@
+package com.example.retry.proxy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class Proxy {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Retryable(value = RuntimeException.class, maxAttempts = 4, backoff = @Backoff(multiplier = 2))
+	public String retry(String pathVar) {
+
+		System.out.println("entering retry :  path var : "+pathVar);
+		if(pathVar.equals("err")) {
+			throw new RuntimeException();
+		}
+		return restTemplate.getForObject("http://httpbin.org/delay/3",String.class);
+	}
+}
